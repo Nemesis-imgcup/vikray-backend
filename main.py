cb388bbbf40520cc0e82d7f814d24158a02db477
Arindam Sahoo <arindams2002@gmail.com>
2022-12-25 09:47:20 +0530

    Initial Push

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-from typing import Union
-
 from fastapi import FastAPI
+import db
+import models
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "Hello Nemesis, I am Vikray    नमस्कार, विक्रय में आपका स्वागत है"}
 
-@app.get("/api/v1/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/all")
+def get_all():
+    data = db.all()
+    return {"data":data}
+
+@app.get("/allusernames")
+def get_allusernames():
+    data = db.all_usernames()
+    return {"data":data}
+
+@app.post("/create")
+def create(data: models.Users):
+    data = models.Users(username=data.username, password=data.password)
+    id = db.create(data)
+    return {"inserted": True, "inserted_id": id}
+
+@app.get("/get/{username}")
+def get_one(username:str):
+    data = db.get_one(username)
+    return {"data":data}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.delete("/delete")
+def delete(username:str):
+    data = db.delete(username)
+    return {"deleted": True, "deleted_count": data}
+
+@app.put("/update")
+def update(data:models.Users):
+    data = db.update(data)
+    return {"updated": True, "updated_count": data}
